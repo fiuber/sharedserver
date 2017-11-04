efd061f2aaed28505c85ee4811e1c122e7a82c80
soyyo5159 <soyyo5159@hotmail.com>
2017-11-03 22:12:28 -0300

    [1h] primera versión de /trips/estimate, sin testear ni nada parecido, casi un borrador

--- src/routes/model/trips.js
+++ src/routes/model/trips.js
@@ -96,6 +96,161 @@ exports.getUserTrips=function(userId,nonexistent,badRevision,me){
     })
 }
 
-exports.estimate=function(){
-    return {sure:"yes"};
+exports.estimate=function(body,nonexistent,badRevision,me){
+    /*
+    {
+        "id": "string",
+        "applicationOwner": "string",
+        "driver": "string",
+        "passenger": "string",
+        "start": null,
+        "end": null,
+        "totalTime": 0,
+        "waitTime": 0,
+        "travelTime": 0,
+        "distance": 0,
+        "route": null,
+        "cost": null
+    }
+    */
+    //Características del conductor (viajes en el día, viajes en el mes, antigüedad)
+    //Cantidad de viajes que se realizaron en la última ventana temporal (Hora, 30 mins, 10 mins)
+    return Promise.resolve(true)
+    .then(()=>{
+        return fullDataFrom(body.driver)
+    }).then((fd)=>{
+        body.driver=fd;
+    })
+    //Características del pasajero (viajes en el día, viajes en el mes, antigüedad, saldo)
+    //Cantidad de viajes que se realizaron en la última ventana temporal (Hora, 30 mins, 10 mins)
+    .then(()=>{
+        return fullDataFrom(body.passenger)
+    }).then((fd)=>{
+        body.passenger=fd;
+        return Promise.resolve(true)
+    })
+    //Método de pago
+    .then(()=>{
+        return payer.paymentMethods();
+    }).then((methods)=>{
+        body.paymentMethod=methods[0].paymethod;
+        return Promise.resolve(true)
+    })
+    //Características del viaje (duración, distancia, posición geográfica, fecha y hora)[CAMBIA]
+    //Día y horario de la realización del viaje[CAMBIA?]
+    //Tiempo de espera del pasajero para: 
+    //(Que un conductor le confirme el viaje) [CAMBIA]
+    //(Que el conductor llegue a buscarlo) [CAMBIA]
+    .then(()=>{
+        body.travelTime=48*60;//48 min siempre
+        body.waitTime=5*60;//5 min siempre
+        body.totalTime=40*60;//40 min siempre
+        //quedan 3 de espera a que el driver confirme
+
+        body.distance=body.distance || 1200;//1200 siempre
+        body.date=new Date();
+        return Promise.resolve(true)
+    })
+    //Application server que realiza la cotización
+    .then(()=>{
+        return require("./servers").serverIdFromToken(me.token);  
+    }).then((serverId)=>{
+        body.serverId=serverId;
+        return Promise.resolve(true)
+    })
+    //CONCLUSION
+    .then(()=>{
+        return body;
+    })
+
+
+}
+
+
+function fullDataFrom(userId,nonexistent){
+    let user=null;
+    return usersModel.get(userId,nonexistent).then((u)=>{
+        user=u;
+        return Promise.resolve(true);
+    }).then(()=>{
+        return tripsThisMonth(userId).then((thisMonth)=>{
+            user.tripsThisMonth=thisMonth;
+            return Promise.resolve(true);
+        })
+    }).then(()=>{
+        return tripsToday(userId).then((today)=>{
+            user.tripsToday=today;
+            return Promise.resolve(true);
+        })
+    }).then(()=>{
+        return antiqueness(userId).then((antiqueness)=>{
+            user.antiqueness=antiqueness;
+            return Promise.resolve(true);
+        })
+    }).then(()=>{
+        return tripsLastHour(userId).then((tripsLastHour)=>{
+            user.tripsLastHour=tripsLastHour;
+            return Promise.resolve(true);
+        })
+    }).then(()=>{
+        return tripsLast30m(userId).then((tripsLast30m)=>{
+            user.tripsLast30m=tripsLast30m;
+            return Promise.resolve(true);
+        })
+    }).then(()=>{
+        return tripsLast10m(userId).then((tripsLast10m)=>{
+            user.tripsLast10m=tripsLast10m;
+            return Promise.resolve(true);
+        })
+    })
+}
+
+function tripsThisMonth(userId){
+    let date=new Date();
+    let y=date.getFullYear();
+    let m=date.getMonth();
+    let beginningOfMonth=new Date(y,m,0);
+    return tripsSince(userId,beginningOfMonth);
+}
+
+function tripsToday(userId){
+    let beginningOfDay=new Date();
+    beginningOfDay.setHours(0,0,0,0);
+    return tripsSince(userId,beginningOfDay);
+}
+
+function antiqueness(userId){//hay que empezar a guardar esto en la tabla
+    //ponele que empezaron todos este mes
+    let date=new Date();
+    let y=date.getFullYear();
+    let m=date.getMonth();
+    let beginningOfMonth=new Date(y,m,0);
+    return (new Date()).getTime()-beginningOfMonth.getTime();
+}
+
+function tripsLastHour(userId){
+    let date=new Date();
+    date.setHours(date.getHours()-1);
+    return tripsSince(userId,date);
+}
+
+function tripsLast30m(userId){
+    let date=new Date();
+    date.setMinutes(date.getMinutes()-30);
+    return tripsSice(userId,date);
+}
+
+function tripsLast10m(userId){
+    let date=new Date();
+    date.setMinutes(date.getMinutes()-10);
+    return tripsSice(userId,date);
+}
+
+
+function tripsSince(userId,date){
+    return tripsModel.getUserTrips(userId,{},{},{}).then((trips)=>{
+        return trips.filter((trip)=>{
+            return trip.endTimestamp>date.getTime();
+        })
+    })
 }
