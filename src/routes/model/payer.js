d4ba5b1e829228e304dca3380607a58b62e8aed8
soyyo5159 <soyyo5159@hotmail.com>
2017-11-05 11:00:15 -0300

    [40m] Agrego json-server y el db.json que emula  la api de pagos

--- src/routes/model/payer.js
+++ src/routes/model/payer.js
@@ -1,3 +1,6 @@
 exports.pay=function(paymethod,value){
     return Promise.resolve(true);
 }
+console.log("-----------------------------------------");
+console.log(process.env.PAYMENT_SERVER);
+console.log("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
