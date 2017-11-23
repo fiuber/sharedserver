7bd451878de0557bde958df5bee8d2d11622d854
soyyo5159 <soyyo5159@hotmail.com>
2017-11-23 09:47:13 -0300

    [1h30m] Pantalla de edición de rules hermosa

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -46781,6 +46781,11 @@ var RuleEditor = exports.RuleEditor = function (_React$Component) {
     }
 
     _createClass(RuleEditor, [{
+        key: 'onClick',
+        value: function onClick(event) {
+            console.log("run!");
+        }
+    }, {
         key: 'render',
         value: function render() {
             return _react2.default.createElement(
@@ -46797,10 +46802,187 @@ var RuleEditor = exports.RuleEditor = function (_React$Component) {
                 ),
                 _react2.default.createElement(
                     'div',
-                    { style: { display: "flex", flexBasis: "60%" } },
-                    'holi'
+                    { style: {
+                            display: "flex",
+                            flexBasis: "60%",
+                            alignItems: "center",
+                            flexDirection: "column",
+                            margin: "10px"
+                        } },
+                    _react2.default.createElement(
+                        'h3',
+                        null,
+                        'Fact to be sent'
+                    ),
+                    _react2.default.createElement(
+                        'div',
+                        { id: 'sent', style: { height: "300px", margin: "10px", width: "100%" } },
+                        'asdddd'
+                    ),
+                    _react2.default.createElement(
+                        'div',
+                        { 'class': 'panel panel-default', style: { width: "100%" } },
+                        _react2.default.createElement(
+                            'div',
+                            { 'class': 'panel-heading' },
+                            _react2.default.createElement(
+                                'h4',
+                                { 'class': 'panel-title' },
+                                _react2.default.createElement(
+                                    'a',
+                                    { 'data-toggle': 'collapse', href: '#collapse2' },
+                                    '"this" object reference'
+                                )
+                            )
+                        ),
+                        _react2.default.createElement(
+                            'div',
+                            { id: 'collapse2', 'class': 'panel-collapse collapse' },
+                            _react2.default.createElement(
+                                'div',
+                                { 'class': 'panel-body' },
+                                _react2.default.createElement(
+                                    'div',
+                                    { id: 'thisReference', style: {
+                                            width: "100%",
+                                            height: "400px"
+                                        } },
+                                    JSON.stringify({ id: 'string',
+                                        applicationOwner: 1,
+                                        driver: { id: 1,
+                                            _ref: '229.06346204048654',
+                                            applicationOwner: '1',
+                                            password: '5159',
+                                            type: 'passenger',
+                                            username: 'soyyo5159',
+                                            name: 'José',
+                                            surname: 'Ignacio',
+                                            country: 'Argentina',
+                                            email: 'soyyo5159@hotmail.com',
+                                            birthdate: 'oneday',
+                                            fbUserId: null,
+                                            fbAuthToken: null,
+                                            balance: [],
+                                            cars: [],
+                                            images: ['muy serio.png', 'playa.png', 'serio.png'],
+                                            tripsThisMonth: 0,
+                                            tripsToday: 0,
+                                            antiqueness: 393339611,
+                                            tripsLastHour: 0,
+                                            tripsLast30m: 0,
+                                            tripsLast10m: 0 },
+                                        passenger: { id: 2,
+                                            _ref: '427.3095411010006',
+                                            applicationOwner: '1',
+                                            password: null,
+                                            type: 'passenger',
+                                            username: 'fayo5159',
+                                            name: 'Facé',
+                                            surname: 'Igfacio',
+                                            country: 'Argentina',
+                                            email: 'soyyo5159@hotmail.com',
+                                            birthdate: 'oneday',
+                                            fbUserId: 'face',
+                                            fbAuthToken: 'face',
+                                            balance: [],
+                                            cars: [],
+                                            images: ['muy serio.png', 'playa.png', 'serio.png'],
+                                            tripsThisMonth: 0,
+                                            tripsToday: 0,
+                                            antiqueness: 393339684,
+                                            tripsLastHour: 0,
+                                            tripsLast30m: 0,
+                                            tripsLast10m: 0 },
+                                        start: { timestamp: 4578999874,
+                                            address: { street: 'las heras', location: [Object] } },
+                                        end: { timestamp: 4865454687,
+                                            address: { street: 'zeballos', location: [Object] } },
+                                        totalTime: 2400,
+                                        waitTime: 300,
+                                        travelTime: 2880,
+                                        distance: 1200,
+                                        route: [],
+                                        cost: 1000,
+                                        date: '2017-11-04T13:15:39.727Z',
+                                        result: true }, null, 2)
+                                )
+                            )
+                        )
+                    ),
+                    _react2.default.createElement(
+                        'button',
+                        { 'class': 'btn btn-primary', onClick: this.onClick.bind(this) },
+                        'Run selected rules'
+                    ),
+                    _react2.default.createElement(
+                        'h3',
+                        null,
+                        'Fact received'
+                    ),
+                    _react2.default.createElement('div', { id: 'received', style: { height: "300px", margin: "10px", width: "100%" } }),
+                    _react2.default.createElement(
+                        'div',
+                        { 'class': 'panel panel-default', style: { width: "100%" } },
+                        _react2.default.createElement(
+                            'div',
+                            { 'class': 'panel-heading' },
+                            _react2.default.createElement(
+                                'h4',
+                                { 'class': 'panel-title' },
+                                _react2.default.createElement(
+                                    'a',
+                                    { 'data-toggle': 'collapse', href: '#collapse1' },
+                                    'Rule reference'
+                                )
+                            )
+                        ),
+                        _react2.default.createElement(
+                            'div',
+                            { id: 'collapse1', 'class': 'panel-collapse collapse' },
+                            _react2.default.createElement(
+                                'div',
+                                { 'class': 'panel-body' },
+                                _react2.default.createElement(
+                                    'div',
+                                    { id: 'ruleReference', style: {
+                                            width: "100%",
+                                            height: "180px"
+                                        } },
+                                    JSON.stringify({
+                                        "name": "transaction minimum",
+                                        "priority": 3,
+                                        "on": true,
+                                        "condition": "function(R) {" + "R.when(this.transactionTotal < 500);" + "}",
+                                        "consequence": "function(R) {" + "this.result = false;" + "R.stop();//R.restart()//R.next()" + "}"
+                                    }, null, 2)
+                                )
+                            )
+                        )
+                    )
                 )
             );
+        }
+    }, {
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+            var sent = ace.edit("sent");
+            sent.setTheme("ace/theme/clouds");
+            sent.getSession().setMode("ace/mode/json");
+
+            var received = ace.edit("received");
+            received.setTheme("ace/theme/clouds");
+            received.getSession().setMode("ace/mode/json");
+            received.setReadOnly(true);
+
+            var thisReference = ace.edit("thisReference");
+            thisReference.setTheme("ace/theme/clouds");
+            thisReference.getSession().setMode("ace/mode/json");
+            thisReference.setReadOnly(true);
+
+            var ruleReference = ace.edit("ruleReference");
+            ruleReference.setTheme("ace/theme/clouds");
+            ruleReference.getSession().setMode("ace/mode/json");
+            ruleReference.setReadOnly(true);
         }
     }, {
         key: 'selectionCallback',
