755b27234bc92aed6848237b4f59f5a290861c2a
soyyo5159 <soyyo5159@hotmail.com>
2017-11-26 18:51:32 -0300

    [30m] agrego mejores errores 400

--- src/routes/expressify/apify/satisfiesShape-test.js
+++ src/routes/expressify/apify/satisfiesShape-test.js
@@ -7,34 +7,42 @@ describe("funcionamiento de satisfiesShape",function(){
         assert(satisfiesShape(
             {a:"x",b:"y"},
             {a:"z",b:"q"}
-        ))
+        ).valueOf())
     })
 
     it("JSON simples, uno mayor que el otro",function(){
         assert(satisfiesShape(
             {a:"x",b:"y",k:"k"},
             {a:"z",b:"q"}
-        ))
+        ).valueOf())
     })
 
     it("JSON con más JSON adentro",function(){
         assert(satisfiesShape(
             {a:"x",b:{c:"c",d:"d"},k:"k"},
             {a:"z",b:{c:"p",d:"q"}}
-        ))
+        ).valueOf())
     })
 
     it("JSON simples no tienen la misma forma",function(){
         assert(!satisfiesShape(
             {a:"x",b:{c:"c",d:"d"},k:"k"},
             {t:"z"}
-        ))
+        ).valueOf())
     })
 
     it("JSON complejos no tienen la misma forma",function(){
-        assert(!satisfiesShape(
+        let v=satisfiesShape(
             {a:"x",b:{c:"c",d:"d"},k:"k"},
             {a:"z",b:{p:"q"}}
-        ))
+        )
+        console.log("8888888888888888")
+        console.log("8888888888888888")
+        console.log("8888888888888888")
+        console.log(v)
+        console.log("8888888888888888")
+        console.log("8888888888888888")
+        console.log("8888888888888888")
+        assert(!v)
     })
 })
